953a6c78421639c2e50439e6a3f9f8188f831eb1
willeyao <willeyao@163.com>
2022-05-31 13:55:52 +0800

    feat:add keepDecimal function

--- src/com/yao/util/NumberUtil.java
+++ src/com/yao/util/NumberUtil.java
@@ -17,4 +17,25 @@ public class NumberUtil {
 	public static void main(String[] args){
 		System.out.println(random(100));
 	}
+	
+     /**
+       * 经纬度小数点要保留的长度
+       */
+       public final static Integer LNGLAT_PRECISION = 6;
+
+       public static String toString(Integer num){
+          return num == null ? null : num.toString();
+       }
+
+       public static Double keepLngLatDecimal(Double num){
+          return keepDecimal(num, LNGLAT_PRECISION);
+       }
+       public static Double keepDecimal(Double num, Integer precision){
+          if (num == null) {
+              return null;
+          }
+          BigDecimal b = new BigDecimal(num);
+          num = b.setScale(precision, BigDecimal.ROUND_HALF_UP).doubleValue();
+          return num;
+      }
 }
